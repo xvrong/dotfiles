c2ad96363f68cf5b06e6028d8b08e437e8cbd906
xv_rong <rongsheng_qv@outlook.com>
2022-12-30 15:37:11 +0800

    更改了一下快捷键

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -66,7 +66,7 @@ local autorun = {
     cmd = {
         "fcitx5",
         "picom --daemon",
-        "~/Projects/Script/python_venv/bin/python ~/Projects/Script/link_dlut.py",
+        -- "~/Projects/Script/python_venv/bin/python ~/Projects/Script/link_dlut.py",
         "greenclip daemon"
     }
 }
@@ -333,20 +333,22 @@ awful.keyboard.append_global_keybindings({
 
 -- Layout related keybindings
 awful.keyboard.append_global_keybindings({
-    awful.key({ modkey, "Shift" }, "j", function() awful.client.swap.byidx(1) end,
+    awful.key({ modkey, }, "h", function() awful.client.swap.byidx(1) end,
         { description = "swap with next client by index", group = "client" }),
-    awful.key({ modkey, "Shift" }, "k", function() awful.client.swap.byidx(-1) end,
+    awful.key({ modkey, }, "l", function() awful.client.swap.byidx(-1) end,
         { description = "swap with previous client by index", group = "client" }),
     -- awful.key({ modkey, }, "u", awful.client.urgent.jumpto,
     --     { description = "jump to urgent client", group = "client" }),
-    awful.key({ modkey, }, "l", function() awful.tag.incmwfact(0.05) end,
+    awful.key({ modkey, "Shift" }, "l", function() awful.tag.incmwfact(0.05) end,
         { description = "increase master width factor", group = "layout" }),
-    awful.key({ modkey, }, "h", function() awful.tag.incmwfact(-0.05) end,
+    awful.key({ modkey, "Shift" }, "h", function() awful.tag.incmwfact(-0.05) end,
         { description = "decrease master width factor", group = "layout" }),
-    awful.key({ modkey, "Shift" }, "l", function() awful.client.incwfact(0.10) end,
+    awful.key({ modkey, "Shift" }, "j", function() awful.client.incwfact(0.10) end,
         { description = "increase window factor of client", group = "layout" }),
-    awful.key({ modkey, "Shift" }, "h", function() awful.client.incwfact(-0.10) end,
+    awful.key({ modkey, "Shift" }, "k", function() awful.client.incwfact(-0.10) end,
         { description = "decrease window factor of client", group = "layout" }),
+    awful.key({ modkey, "Shift" }, "space", function() awful.layout.inc(1) end,
+        { description = "select previous", group = "layout" }),
     --floatingbutton awful.key({ modkey, "Shift" }, "l", function() awful.tag.incnmaster(-1, nil, true) end,
     --     { description = "decrease the number of master clients", group = "layout" }),
     -- awful.key({ modkey, "Control" }, "h", function() awful.tag.incncol(1, nil, true) end,
@@ -355,8 +357,7 @@ awful.keyboard.append_global_keybindings({
     --     { description = "decrease the number of columns", group = "layout" }),
     -- awful.key({ modkey, }, "space", function() awful.layout.inc(1) end,
     --     { description = "select next", group = "layout" }),
-    awful.key({ modkey, "Shift" }, "space", function() awful.layout.inc(1) end,
-        { description = "select previous", group = "layout" }),
+
 })
 
 
@@ -553,6 +554,11 @@ ruled.client.connect_signal("request::rules", function()
     ruled.client.append_rule {
         rule       = { class = "Microsoft-edge" },
         properties = { tag = tags[2] }
+    }
+
+    ruled.client.append_rule {
+        rule       = { class = "QQ" },
+        properties = { tag = tags[4] }
     }
 end)
 -- }}}
