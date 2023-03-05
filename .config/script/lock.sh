ff90411cea1b94b72c452da10ed0d44322e1a77c
xv_rong <rongsheng_qv@outlook.com>
2023-03-06 00:08:19 +0800

    调整快捷键，增加锁屏功能

---
+++ .config/script/lock.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+swaylock --image /home/xv_rong/Pictures/WallPapers/pixiv55325876.jpg --ignore-empty-password --indicator-caps-lock --key-hl-color 3060D7FF --inside-color 00000000 --inside-clear-color 00000066 --line-color 00000000 --ring-clear-color 00000066 --separator-color 00000040 --line-clear-color 00000000 --line-ver-color 00000000 --line-wrong-color=00000000
