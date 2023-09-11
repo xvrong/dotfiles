5be4ff1027e8142677c0f747acd74bdece09b07c
xv_rong <rongsheng_qv@outlook.com>
2023-09-11 20:07:52 +0800

    fixd: dropdown terminal

--- .config/hypr/scripts/dropdown_specialworkspace.sh
+++ .config/hypr/scripts/dropdown_specialworkspace.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 drop=`ps -aux | grep alacritty-dropdown | grep -v grep`
 if [ -z "$drop" ]; then
-    hyprctl dispatch -- exec '[workspace special dropdown; size 75% 50%] alacritty  --class alacritty-dropdown --command fish'
-    sleep 0.5
-    hyprctl dispatch togglespecialworkspace dropdown
+    hyprctl dispatch -- exec '[workspace special; size 75% 50%] alacritty  --class alacritty-dropdown --command fish'
 else
-    hyprctl dispatch togglespecialworkspace dropdown
+    hyprctl dispatch togglespecialworkspace
 fi
