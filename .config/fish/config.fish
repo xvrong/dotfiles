d1e533565779f8302ff537edcdbfeea87f5c55d1
xv_rong <rongsheng_qv@outlook.com>
2024-03-02 02:07:58 +0800

    fish相关

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,22 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     [ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
 end
+
+fish_vi_key_bindings   
+
+# Created by `pipx` on 2023-11-11 11:06:38
+set PATH $PATH /home/xv_rong/.local/bin
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/miniconda3/bin/conda
+    eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
