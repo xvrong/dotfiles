560ec5a3454770e7298ccdae8c9186f540a44fc1
xv_rong <rongsheng_qv@outlook.com>
2024-03-02 02:11:05 +0800

    fix dropdown terminal not centered and wrong size among movemonitor

--- .config/hypr/scripts/dropdown_specialworkspace.sh
+++ .config/hypr/scripts/dropdown_specialworkspace.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 drop=`ps -aux | grep alacritty-dropdown | grep -v grep`
 if [ -z "$drop" ]; then
-    hyprctl dispatch -- exec '[workspace special; size 75% 50%] alacritty  --class alacritty-dropdown --command fish'
+    hyprctl dispatch -- exec '[workspace special; float;size 60% 75%; center 1] alacritty  --class alacritty-dropdown --command fish'
 else
     hyprctl dispatch togglespecialworkspace
+    hyprctl dispatch resizeactive exact 60% 75%
+    hyprctl dispatch centerwindow 1
 fi
